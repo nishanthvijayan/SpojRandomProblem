3222ae4ed5f78ff70936585e069f8e859b1f3f69
Nishanth Vijayan <nishanththegr8@gmail.com>
2015-03-12 09:59:51 +0530

    fixed minor error

--- api/route.py
+++ api/route.py
@@ -12,11 +12,11 @@ def getData():
     randomNumOne = random.randint(1, 2900)
     page = urllib2.urlopen("http://www.spoj.com/problems/classical/sort=0,start="+str(randomNumOne))
     soup = BeautifulSoup(page,"html.parser")
-    probrows = soup.findAll("tr",attrs = {"class":"problemrow"})
+    probrows = soup.findAll("tr")
     randomNumTwo = random.randint(1,50)
     probrow = probrows[randomNumTwo]
     ID =    str(probrow.select("td")[0].text.strip().encode("ascii","ignore"))
-    CODE = str(probrow.select("td")[2].text.strip().encode("ascii","ignore"))
+    CODE = str(probrow.select("td")[1].a["href"].strip().encode("ascii","ignore"))
     NAME = str(probrow.select("td")[1].text.strip().encode("ascii","ignore"))
     USERS = str(probrow.select("td")[3].text.strip().encode("ascii","ignore"))
     ACCURACY = str(probrow.select("td")[4].text.strip().encode("ascii","ignore"))
@@ -35,5 +35,6 @@ def index():
 
 
 if __name__ == '__main__':
+    #app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
